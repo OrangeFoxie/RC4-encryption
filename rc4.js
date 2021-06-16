8da10e9244ad5542304ae3e5842bc0b7be7761b0
OrangeFoxie <vinhphuc931998@gmai.com>
2021-06-16 11:59:15 +0700

    Encrypt fixed, decode added

--- rc4.js
+++ rc4.js
@@ -1,32 +1,50 @@
 
 function rc4(key, str) {
-	var s = [], j = 0, x, res = '';
-	for (var i = 0; i < 256; i++) {
-		s[i] = i;
-	}
-	for (i = 0; i < 256; i++) {
-		j = (j + s[i] + str.charCodeAt(i % key.length)) % 256;
-		x = s[i];
-		s[i] = s[j];
-		s[j] = x;
-	}
-	i = 0;
-	j = 0;
-	for (var y = 0; y < str.length; y++) {
-		i = (i + 1) % 256;
-		j = (j + s[i]) % 256;
-		x = s[i];
-        
-		s[i] = s[j];
-		s[j] = x;
-		res += String.fromCharCode(str.charCodeAt(y) ^ s[(s[i] + s[j]) % 256]);
-	}
-	return res;
+    s = new Array();
+    for (var i = 0; i < 256; i++) {
+        s[i] = i;
+    };
+    var j = 0;
+    var x;
+    for (i = 0; i < 256; i++) {
+        j = (j + s[i] + key.charCodeAt(i % key.length)) % 256;
+        x = s[i];
+        s[i] = s[j];
+        s[j] = x;
+    }
+    i = 0;
+    j = 0;
+    var ct = '';
+    for (var y = 0; y < str.length; y++) {
+        i = (i + 1) % 256;
+        j = (j + s[i]) % 256;
+        x = s[i];
+        s[i] = s[j];
+        s[j] = x;
+        ct += String.fromCharCode(str.charCodeAt(y) ^ s[(s[i] + s[j]) % 256]);
+    }
+    return ct;
 }
-
+let kq1;
 function rc4Enc(){
     let strInput = document.getElementById("strInput").value;
     let keyEnc = document.getElementById("keyEnc").value;
+    kq1 = rc4(keyEnc,strInput);
+    this.checkkq("txtoutput",kq1);
+}
+
+function rc4Dec(){
+	rc4Enc();
+	let strInput = kq1;
+    let keyEnc = document.getElementById("keyEnc").value;
     let kq = rc4(keyEnc,strInput);
-    document.getElementById("txtoutput").innerHTML = kq;
+	this.checkkq("txtoutput2",kq);
+}
+
+function checkkq(id,kq){
+	if(kq){
+		document.getElementById(id).innerHTML = kq;
+	}else{
+		document.getElementById(id).innerHTML = "Kết quả";
+	}
 }
